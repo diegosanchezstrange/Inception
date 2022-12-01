9d833045721a24ef32a50017824bde0baf99395a
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2022-12-01 16:04:44 +0100

    done

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -2,10 +2,12 @@
 
 wp --allow-root core download
 
-wp --allow-root config create --dbhost=mysql --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS > out 2> oute
+wp --allow-root config create --dbhost=mysql --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS
 
 wp --allow-root db create
 
 wp --allow-root core install --url=dsanchez.42.fr --title=Inception --admin_user=diego --admin_password=password --admin_email=dsanchez@student.42madrid.com
+
+wp --allow-root user create diego dsanchez@student.42madrid.com --user_pass=1234
 
 php-fpm7.3 -F
